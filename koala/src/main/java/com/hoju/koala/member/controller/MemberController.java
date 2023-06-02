d19fb3555f266aacc89b0e7a93379478a63de81a
1000baamK <1000baam@gmail.com>
2023-06-02 12:00:23 +0900

    1000병국

--- koala/src/main/java/com/hoju/koala/member/controller/MemberController.java
+++ koala/src/main/java/com/hoju/koala/member/controller/MemberController.java
@@ -1,19 +1,25 @@
 package com.hoju.koala.member.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hoju.koala.member.model.service.MemberService;
+import com.hoju.koala.member.model.vo.Member;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 	
 	@Autowired
-	private MemberService memberSerivce;
+	private MemberService memberService;
 	
 	
 	
@@ -24,6 +30,22 @@ public class MemberController {
 		return "member/loginPage";
 	}
 	
+	@PostMapping("login")
+	public String login(HttpSession session,
+						Member m,
+						Model model) {
+		
+		
+		Member loginUser = memberService.loginMember(m);
+		
+		
+		
+		
+		
+		
+		
+		return null;
+	}
 	
 	
 }
--- koala/src/main/java/com/hoju/koala/member/model/dao/MemberDao.java
+++ koala/src/main/java/com/hoju/koala/member/model/dao/MemberDao.java
@@ -1,9 +1,18 @@
 package com.hoju.koala.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.hoju.koala.member.model.vo.Member;
 
 @Repository
 public class MemberDao {
+
+	//로그인
+	public Member loginMember(SqlSessionTemplate sqlSession, Member m) {
+		
+		return sqlSession.selectOne("memberMapper.loginMember", m);
+	}
 	
 	
 }
