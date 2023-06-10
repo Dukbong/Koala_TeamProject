147645a3dff66c5e2850f989c3bd84bd679a3bf5
jang <gorae930301@gmail.com>
2023-06-10 22:02:48 +0900

    jang

--- koala/src/main/java/com/hoju/koala/common/exception/ExceptionController.java
+++ koala/src/main/java/com/hoju/koala/common/exception/ExceptionController.java
@@ -1,5 +1,9 @@
 package com.hoju.koala.common.exception;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +22,14 @@ public class ExceptionController {
 		
 		//@ExceptionHandler 어노테이션은 매개변수로 ModelAndview를 지원하지 않는다고 함.
 		ModelAndView mv = new ModelAndView();
+		String[] types = {"admin", "member", "errorboard", "qna", "free"}; // 이건 각자 알아서 들....
+		List<String> typesList = new ArrayList<>(Arrays.asList(types)); // 그냥 심심하니깐 배열을 list로 바꾸고
+		String type = request.getServletPath().substring(1).split("/")[0]; // root를 제외한 가장 첫 url
+		if(typesList.contains(type)) {
+			// 여기서 어디로 돌아갈지 결정
+		}else {
+			// 여기는 무지성 오류
+		}
 		
 		mv.addObject("errorMsg", ex.getMessage());
 		mv.addObject("url", request.getRequestURL());
@@ -40,4 +52,22 @@ public class ExceptionController {
 		
 		return mv;
 	}
+	
+	
+//	
+//	//400 오류 살짝 변경
+//	@ExceptionHandler(NoHandlerFoundException.class)
+//	public ModelAndView error404Handlers(HttpServletRequest request,
+//										NoHandlerFoundException ex) {
+//		
+//		//@ExceptionHandler 어노테이션은 매개변수로 ModelAndview를 지원하지 않는다고 함.
+//		ModelAndView mv = new ModelAndView();
+//		String url = request.getRequestURL().toString();
+//		System.out.println(url);
+//		mv.addObject("errorMsg", ex.getMessage());
+//		mv.addObject("url", request.getRequestURL());
+//		mv.setViewName("common/error");
+//		
+//		return mv;
+//	}
 }
