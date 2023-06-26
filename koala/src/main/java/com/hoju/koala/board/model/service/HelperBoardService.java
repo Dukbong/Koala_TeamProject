ac30656f78ecdf0cfd9761574384f7e9cd9e27c8
1000baamK <1000baam@gmail.com>
2023-06-26 11:35:35 +0900

    helperboard -> setting 레파지토리로 재구성

--- koala/src/main/java/com/hoju/koala/board/model/service/HelperBoardService.java
+++
@@ -1,5 +0,0 @@
-package com.hoju.koala.board.model.service;
-
-public interface HelperBoardService {
-
-}
--- koala/src/main/java/com/hoju/koala/member/cookie/MemberCookie.java
+++ koala/src/main/java/com/hoju/koala/member/cookie/MemberCookie.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class MemberCookie {
 	
-	public static void setCookie(HttpServletResponse response, String name, String value, int maxAge) {	
+	public void setCookie(HttpServletResponse response, String name, String value, int maxAge) {	
 		Cookie cookie = new Cookie(name, value);
         cookie.setMaxAge(maxAge);
         cookie.setPath("/");
         response.addCookie(cookie);
 	}
 	
-	public static void deleteCookie(HttpServletResponse response, String name) {
+	public void deleteCookie(HttpServletResponse response, String name) {
 		Cookie cookie = new Cookie(name, "");
 		cookie.setMaxAge(0);
 		cookie.setPath("/");
