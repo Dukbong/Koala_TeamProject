22afe578f3029948db18946b0f7f1230480c263b
jang <gorae930301@gmail.com>
2023-07-06 11:40:24 +0900

    커서 위치 제어 시도

--- koala/src/main/java/com/hoju/koala/admin/webSocket/Atest.java
+++ koala/src/main/java/com/hoju/koala/admin/webSocket/Atest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,9 @@ public class Atest extends TextWebSocketHandler {
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		
+		System.out.println("????????????????????????????????????????");
+		System.out.println(session.getAttributes().get("loginUser"));
+		System.out.println("????????????????????????????????????????");
 		list.add(session);
 		System.out.println("추가");
 		System.out.println(list);
@@ -41,12 +44,16 @@ public class Atest extends TextWebSocketHandler {
 
 	@Override
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		System.out.println("============================================================");
+		System.out.println(list);
+		System.out.println("============================================================");
 		// Connect
 		ObjectMapper om = new ObjectMapper();
 		WebSocketVO msv = om.readValue((String)message.getPayload(), WebSocketVO.class); // json을 VO기반으로 읽어서 객체로 저장한다.
 		if(msv.getMsg() != null) {
 			textMap.put(msv.getTeamNo(), msv.getMsg());
 		}
+		System.out.println(msv);
 		if(msv.getType().equals("Enter")) {
 			// 접속시 방에서 작성한 가장 최근의 sql문을 보여줘야한다.
 			// 이는 별도의 Map 자료구조로 구성하여 보여주기로 한다.
@@ -56,23 +63,38 @@ public class Atest extends TextWebSocketHandler {
 			
 			HashSet<WebSocketSession> set = map.get(msv.getTeamNo());
 			set.add(session);
+			System.out.println("------------------------------------------------------------");
+			System.out.println(set);
+			System.out.println(set.size());
+			System.out.println(set.getClass().getName()); // hashset 문제 : get 함수가 먹히지 않는다.
+			Iterator<WebSocketSession> iter = set.iterator();	// Iterator 사용
+			while(iter.hasNext()) {//값이 있으면 true 없으면 false
+			    System.out.println(iter.next().getAttributes().get("loginUser"));
+			}
+			System.out.println("------------------------------------------------------------");
 			if(map.get(msv.getTeamNo()).size() == 1) {
 				String content = adminService.selectSqlContent(msv.getTeamNo());
 				textMap.put(msv.getTeamNo(), content);
 			}
 			ArrayList<String> arraylist = new ArrayList<>();
+			System.out.println(arraylist);
 			for(WebSocketSession preId : set) {
-				String userId = ((Member)preId.getAttributes().get("loginUser")).getUserId();
-				arraylist.add(userId);
+				try {
+					String userId = ((Member)preId.getAttributes().get("loginUser")).getUserId();
+					arraylist.add(userId);
+				}catch(Exception e) {
+					
+				}
 			}
+			System.out.println("최종 접속 유저  : " + arraylist);
 			TextMessage newMessage = null;
 			if(textMap.get(msv.getTeamNo()) != null) {
 				newMessage = new TextMessage((arraylist.toString()).replace(" ", "") + "MSG:" +textMap.get(msv.getTeamNo()));
 			}else {				
 				newMessage = new TextMessage((arraylist.toString()).replace(" ", ""));
 			}
-			System.out.println(">>>>>>>");
-			System.out.println(map.get(msv.getTeamNo()));
+//			System.out.println(">>>>>>>");
+//			System.out.println(map.get(msv.getTeamNo()));
 			for(WebSocketSession m : map.get(msv.getTeamNo())) {
 				try {					
 					m.sendMessage(newMessage);
@@ -105,12 +127,17 @@ public class Atest extends TextWebSocketHandler {
 			System.out.println("send >> " + msv.getMsg());
 			String text = msv.getMsg();
 			TextMessage newMessage = new TextMessage("SEND:"+text);
+			System.out.println("sendMember >> " + map.get(msv.getTeamNo()));
 			try {
 				for(WebSocketSession m : map.get(msv.getTeamNo())) {
-					m.sendMessage(newMessage);
+					try {						
+						m.sendMessage(newMessage);
+					}catch(Exception e) {
+						
+					}
 				}
 			}catch(NullPointerException e) {
-				System.out.println("SEND : msv.getTeamNo() : " + msv.getTeamNo());
+				System.out.println("SEND Error : msv.getTeamNo() : " + msv.getTeamNo());
 			}
 		} else if(msv.getType().equals("SAVE:")) {
 			SqlCloud sql = adminService.selectSqlDate(msv.getTeamNo());
